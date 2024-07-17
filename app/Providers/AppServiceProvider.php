e55b8492fe224120198b402ed86ff99dcf39fdaa
omediadon <omediadon@gmail.com>
2024-07-17 03:22:02 +0100

    Disable lazy loading across the application!

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,21 +5,19 @@ namespace App\Providers;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
+class AppServiceProvider extends ServiceProvider{
+	/**
+	 * Register any application services.
+	 */
+	public function register(): void{
 		//
-    }
+	}
+
+	/**
+	 * Bootstrap any application services.
+	 */
+	public function boot(): void{
+		// No Lazy Loading! Power of the habit
+		Model::preventLazyLoading();
+	}
 }
